ac3998d527c55018dd02b50699a4a8afefbbeecd
Евгений Радченко <evgeni.radchenko2000@gmail.com>
2020-11-23 01:45:42 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp28/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prrr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Формула y1 = x + y");
+            Console.ReadKey();
+            double x = 2;
+            double y = 0;
+            while (x <= 10)
+            {
+
+                Console.WriteLine("x = " + x);
+                Console.WriteLine("y = " + y);
+                double y1 = x + y;
+                Console.WriteLine("Результат = " + y1);
+                x += 2;
+                y += 2;
+            }
+            Console.ReadKey();
+        }
+    }
+}
